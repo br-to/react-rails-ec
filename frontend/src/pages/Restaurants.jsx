4efbd8ffecdad3f48a08ffbb56faf0b595995166
br-to <barabara0224@gmail.com>
2021-10-31 19:36:31 +0900

    レストラン一覧ページ作成

--- frontend/src/pages/Restaurants.jsx
+++ frontend/src/pages/Restaurants.jsx
@@ -1,19 +1,109 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import API from '../utils/API';
+import Skeleton from '@material-ui/lab/Skeleton';
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+import RestaurantImage from '../images/restaurant-image.jpg';
+
+import {
+  initialState,
+  restaurantsActionTypes,
+  restaurantsReducer,
+} from '../reducers/restaurants';
+import { REQUEST_STATE } from '../constants/constant';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
+
+const RestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+
+const RestaurantsContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+  padding: 48px;
+`;
+
+const RestaurantsImageNode = styled.img`
+  width: 100%;
+`;
+
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
 
 const Restaurants = () => {
-  const [restaurants, setRestaurants] = useState([]);
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
 
   useEffect(async () => {
+    dispatch({ type: restaurantsActionTypes.FETCHING });
     await API.fetchRestaurants().then((res) => {
-      setRestaurants(res.data.restaurants);
+      dispatch({
+        type: restaurantsActionTypes.FETCH_SUCCESS,
+        payload: {
+          restaurants: res.data.restaurants,
+        },
+      });
     });
   }, []);
 
   return (
     <>
-      レストラン一覧
-      <p>{JSON.stringify(restaurants)}</p>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+      <RestaurantsContentsList>
+        {state.fetchState === REQUEST_STATE.LOADING ? (
+          <>
+            <Skeleton variant="rect" width={450} height={300} />
+            <Skeleton variant="rect" width={450} height={300} />
+            <Skeleton variant="rect" width={450} height={300} />
+          </>
+        ) : (
+          state.restaurantsList.map((item, index) => (
+            <Link
+              to={`/restaurants/${item.id}/foods`}
+              key={index}
+              style={{ textDecoration: 'none' }}
+            >
+              <RestaurantsContentWrapper>
+                <RestaurantsImageNode src={RestaurantImage} />
+                <MainText>{item.name}</MainText>
+                <SubText>{`配送料: ${item.fee}円 ${item.time_required}分`}</SubText>
+              </RestaurantsContentWrapper>
+            </Link>
+          ))
+        )}
+      </RestaurantsContentsList>
     </>
   );
 };
